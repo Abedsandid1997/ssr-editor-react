08d13683a3f93f42f6653bffe4b4bc17487b44a5
Abdelrman <absn23@studenr.bth.se>
2025-10-15 01:29:38 +0200

    refactoring the code

--- middleware.ts
+++ middleware.ts
@@ -3,13 +3,13 @@ import type { NextRequest } from "next/server";
 import { verfiyAuth } from "./lib/auth";
 
 export async function middleware(req: NextRequest) {
-  const token = req.cookies.get("token")?.value;
-  console.log(token, "hhhhhhhhhhhhhhhhhhhhhhhh");
-  if (!token) {
-    return NextResponse.redirect(
-      new URL(`/signin?redirect=${req.url}`, req.url)
-    );
-  }
+  //   const token = req.cookies.get("token")?.value;
+  //   console.log(token, "hhhhhhhhhhhhhhhhhhhhhhhh");
+  //   if (!token) {
+  //     return NextResponse.redirect(
+  //       new URL(`/signin?redirect=${req.url}`, req.url)
+  //     );
+  //   }
 
   //   const verfiedToken = await verfiyAuth(token).catch((err) => {
   //     console.log(err);
